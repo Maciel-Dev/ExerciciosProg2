8cbd5b4fc85d6b8f07ee5bded0485a2f76d4af6e
Maciel-dev <jvmacielv@gmail.com>
2021-09-30 17:59:00 -0300

    .

---
+++ Exercicio2.py
@@ -0,0 +1,13 @@
+def main():
+    lista = []
+
+    for i in range(10):
+        userNum = float(input("Digite um numero qualquer: "))
+        lista.append(userNum/2)
+
+    print(lista)
+
+
+
+if __name__ == "__main__":
+    main()
