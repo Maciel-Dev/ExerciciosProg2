ae98a04b0d202e98e95615e8ac37f45c85f6229b
Maciel-dev <jvmacielv@gmail.com>
2021-09-30 17:56:37 -0300

    .

---
+++ Exercicio 1.py
@@ -0,0 +1,28 @@
+def fRetornaLista(numeros):
+    #Retorna apenas numeros pares
+    #Loop
+    lista = []
+    for i in range(numeros + 1):
+        if i%2 == 0:
+            lista.insert(0, i)
+
+
+    #Retorno
+    return lista
+
+
+
+
+
+def main():
+
+    inputUsuario = int(input())
+
+
+    listaRetorno = fRetornaLista(inputUsuario)
+    print(listaRetorno)
+
+
+
+if __name__ == "__main__":
+    main()
